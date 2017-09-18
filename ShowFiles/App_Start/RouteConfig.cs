aa6be046e4693cdd9f8255abd9fcf96d7ce27be5
Kent Huang <kenttttt0@gmail.com>
2017-09-17 23:26:28 -0700

    added mvc app

--- ShowFiles/App_Start/RouteConfig.cs
+++ ShowFiles/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace ShowFiles
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Files",
+                url: "Files/{action}/{filename}",
+                defaults: new {controller = "Files", action="Index", filename = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
---
+++ ShowFiles/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShowFiles.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            Array.Sort(files);
+            return View(files);
+        }
+
+        public ActionResult Filecontent(string filename)
+        {
+            var filepath = Server.MapPath($"~/TextFiles/{filename}.txt");
+            string c;
+            if (System.IO.File.Exists(filepath))
+            {
+                c = System.IO.File.ReadAllText(filepath);
+                
+            } else
+            {
+                c = "File Not Found";
+            }
+            
+            return View((object)c);
+            
+                
+            
+        }
+    }
+}
